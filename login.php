1ce50f3dedf207d7f2399836586b5da36e89bc92
Gustavo Spengler <84518613+gustavospe@users.noreply.github.com>
2021-11-28 19:19:03 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>login Page</title>
+</head>
+<body>
+	
+	<div class="table">
+		
+		<h1>Login</h1>
+		<br>
+		<input type="text" placeholder="Usuário">
+		<br><br>
+		<input type="password" placeholder="senha">
+		<br><br>
+		<button>Enviar</button>
+
+	</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/register.css">
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>register Page</title>
+</head>
+<body>
+	
+	<div class="table">
+		
+		<h1>Cadastre-se</h1>
+		<br>
+		<input type="text" placeholder="Usuário">
+		<br><br>
+		<input type="password" placeholder="senha">
+		<br><br>
+		<input type="password" placeholder="Confirme sua senha">
+		<br><br>
+		<button>Enviar</button>
+
+	</div>
+
+</body>
+</html>
